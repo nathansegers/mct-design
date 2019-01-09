c9ad34f0418045606b1e9dc11adc36c770a421a5
Simon Coudeville <coudeville.simon@gmail.com>
2019-01-09 09:44:51 +0100

    Added javascript

---
+++ themes/mct/static/js/theme.concat.js
@@ -0,0 +1,84 @@
+var chopstick =
+{
+    // init, something like a constructor
+    init: function()
+    {
+        chopstick.loadObject(chopstick.mobileNav, 'chopstick.mobileNav');
+
+        var page = $(".js-page");
+        var choice = $(".js-curriculum-choice");
+        var choiceTop = choice.offset().top;
+
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= choiceTop) {
+                page.addClass("has-scrolled");
+            } else {
+                page.removeClass("has-scrolled");
+            }
+        });
+    },
+
+    /**
+     * This function will load an object by a given name
+     *
+     * If the object doesn't exist no error will be thrown
+     * But if object exists but doesn't have an init method it will throw an error
+     *
+     * If everything is ok we'll initiate the given object
+     */
+    loadObject: function(obj, name)
+    {
+        // create object based on a name
+        // var objName = window[objName];
+
+        // check if object exists
+        if(typeof obj != 'undefined') {
+
+            // check if object has a method init
+            if (typeof obj.init == 'undefined') {
+                // we will throw an error so the designer / developer know there's a problem
+                throw new Error('ERROR: "' + name + '" does not have an init function');
+
+            } else {
+                // everything is fine so initiate
+                obj.init();
+            }
+        }
+    }
+};
+
+var mobileNavSettings
+chopstick.mobileNav =
+{
+    settings:
+    {
+        navigation: $('.js-nav-mobile'),
+        trigger: $('.js-nav-trigger')
+    },
+
+    init: function()
+    {
+        // Initialize mobile nav settings
+        mobileNavSettings = chopstick.mobileNav.settings;
+        // Bind toggle events
+        chopstick.mobileNav.bindUIEvents();
+    },
+
+    bindUIEvents: function()
+    {
+        mobileNavSettings.trigger.on('click', function() {
+            chopstick.mobileNav.toggleNavigation();
+        });
+    },
+
+    // build mobile nav
+    toggleNavigation: function()
+    {
+        mobileNavSettings.navigation.toggleClass('is-visible');
+        mobileNavSettings.trigger.toggleClass('is-active');
+    }
+};
+
+$(chopstick.init);
