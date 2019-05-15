de57d2f41176b3f91b2ba7f32bd4873abe6dcbc7
Simon <simon.coudeville@howest.be>
2019-05-15 08:25:15 +0200

    Added dialog component

--- themes/mct/assets/js/modal.js
+++ themes/mct/assets/js/modal.js
@@ -1,15 +1,25 @@
 const modal = (function() {
 	console.log('Ready to enable Modal');
 	const setup = function() {
-		const modules = document.querySelectorAll('.c-module--link');
+		const modules = document.querySelectorAll('.js-module-link');
+		const closeBtn = document.querySelector('.js-dialog-close');
+		const modal = document.querySelector('.js-dialog');
+		const body = document.querySelector('body');
 
 		if (modules) {
 			for (const m of modules) {
 				m.addEventListener('click', function(e) {
 					e.preventDefault();
 					console.log(m);
+					modal.showModal();
+					body.classList.add("has-modal");
 				});
 			}
+			closeBtn.addEventListener('click', () => {
+				// + escape toets ook
+				modal.close();
+				body.classList.remove("has-modal");
+			});
 		}
 	};
 
